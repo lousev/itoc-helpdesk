bd7ae07aee8b2bca677cd318e7d1879590713c67
lousev <louiesevilla21@gmail.com>
2019-08-18 18:28:50 +0800

    added files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Looping through the Navigation List
+const navList = document.querySelectorAll(".nav-list");
+for (let i = 0; i < navList.length; i++) {
+  navList[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// Initializing Swup
+const swup = new Swup();
